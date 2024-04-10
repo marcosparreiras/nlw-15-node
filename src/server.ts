64f0c63c9d56dd2a19257994b33fce14c5da99ad
Marcos Parreiras <marcosalp1998@gmail.com>
2024-04-10 00:56:04 -0300

    feat: add create-event route and add zod dependency

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,34 @@
 import fastify from "fastify";
+import { z } from "zod";
+import { PrismaClient } from "@prisma/client";
 
 const app = fastify();
 
-app.post("/events", (request, reply) => {
-  console.log(request.body);
+const prisma = new PrismaClient({
+  log: ["query"],
+});
 
-  reply.status(201).send({ success: true });
+app.post("/events", async (request, reply) => {
+  const requestBodySchema = z.object({
+    title: z.string().min(4),
+    details: z.string().nullable(),
+    maximumAttendees: z.number().int().positive().nullable(),
+  });
+
+  const { title, details, maximumAttendees } = requestBodySchema.parse(
+    request.body
+  );
+
+  const event = await prisma.event.create({
+    data: {
+      title,
+      details,
+      maximumAttendees,
+      slug: new Date().toISOString(), // temporary fix
+    },
+  });
+
+  reply.status(201).send({ eventId: event.id });
 });
 
 app.listen({ port: 3000 }).then(() => {
