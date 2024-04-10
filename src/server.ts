b5a0dbc08e918437f9f9ed8bab416170560fab9d
Marcos Parreiras <marcosalp1998@gmail.com>
2024-04-10 00:42:42 -0300

    config: add prisma dependency and prisma schema

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,12 @@ import fastify from "fastify";
 
 const app = fastify();
 
+app.post("/events", (request, reply) => {
+  console.log(request.body);
+
+  reply.status(201).send({ success: true });
+});
+
 app.listen({ port: 3000 }).then(() => {
-  console.log(`Server is running on port ${3000}`);
+  console.log(`HTTP server is running on port ${3000}`);
 });
