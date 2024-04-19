08717b69450b4c0f9311014fb251dd9fc6e55fd9
Marcos Parreiras <marcosalp1998@gmail.com>
2024-04-19 14:59:57 -0300

    test: add fetch-event-attendees e2e-test

---
+++ src/http/routes/fetch-event-attendees.e2e-spec.ts
@@ -0,0 +1,36 @@
+import app from "../app";
+import request from "supertest";
+import { PrismaFakeEntityFactory } from "../../../test/factories/prisma/prisma-fake-entity-factory";
+
+describe("[GET] /events/:eventId/attendees", () => {
+  let prismaFakeEntityFactory: PrismaFakeEntityFactory;
+  beforeAll(async () => {
+    await app.ready();
+    prismaFakeEntityFactory = new PrismaFakeEntityFactory();
+  });
+
+  afterAll(async () => {
+    await app.close();
+    await prismaFakeEntityFactory.disconnect();
+  });
+
+  it("Should be able to create an event", async () => {
+    const events = await Promise.all([
+      await prismaFakeEntityFactory.makeEvent(),
+      await prismaFakeEntityFactory.makeEvent(),
+    ]);
+
+    await Promise.all([
+      prismaFakeEntityFactory.makeAttendee({ eventId: events[0].id }),
+      prismaFakeEntityFactory.makeAttendee({ eventId: events[0].id }),
+      prismaFakeEntityFactory.makeAttendee({ eventId: events[1].id }),
+    ]);
+
+    const response = await request(app.server)
+      .get(`/events/${events[0].id.toString()}/attendees`)
+      .send();
+
+    expect(response.status).toEqual(200);
+    expect(response.body.attendees).toHaveLength(2);
+  });
+});
